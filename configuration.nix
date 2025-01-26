80a630e8f21b7a53155b814dd2fa69908286a0bf
SylviaWhittle <sylviwhittle@gmail.com>
2025-01-26 21:51:30 +0000

    add xwayland to hyprland

--- configuration.nix
+++ configuration.nix
@@ -99,6 +99,7 @@
   programs.hyprland = {
     enable = true;
     withUWSM = true;
+    xwayland.enable = true;
   };
   services.displayManager = {
     sddm = {
--- flake.nix
+++ flake.nix
@@ -3,6 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    hyprland.url = "github:hyprwm/Hyprland";
     hyprpanel.url = "github:Jas-SinghFSU/HyprPanel";
     home-manager = {
       url = "github:nix-community/home-manager";
